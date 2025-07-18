5446cba26d8b2e68268c743c4fc7eda55440a6cf
24B11AI077 <dadisettijayanth7@gmail.com>
2025-07-18 09:45:02 +0530

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,51 @@
+#define STACK_H
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct stack{
+	vector<int>stack;
+	int val;
+	void push(int val){
+		stack.push_back(val);	
+	}
+	void pop()
+	{
+		stack.pop_back();
+	}
+	bool is_empty()
+	{
+		if(stack.empty()){
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	int top()
+	{
+		if(is_empty())
+		{
+			cout << "stack is empty" <<endl;
+			return 0;
+		}
+		else
+		{
+			return cout << stack.back();
+		}
+	}
+	int size()
+	{
+		if(!stack.empty())
+		{
+			return stack.size();	
+		}
+		else
+		{
+			cout << "stack is empty" << endl;
+			return 0;	
+		}
+	}
+
+};
